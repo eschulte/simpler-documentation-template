d45b9dde17338983450d59352c52411bf34ba18c
Eric Schulte <schulte.eric@gmail.com>
2013-01-13 10:41:33 -0700

    customizing

--- specials.lisp
+++ specials.lisp
@@ -27,7 +27,7 @@
 ;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :documentation-template)
+(in-package :schulte-documentation-template)
 
 (defvar *target* nil
   "Where to output the HTML page.  If this value is not NIL, it will
